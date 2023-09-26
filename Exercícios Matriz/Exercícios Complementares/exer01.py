7cbb9f81bdd8634f9050636505326c15af291e00
NeugeMa <mariana.n.dourado@gmail.com>
2023-09-26 10:31:42 -0300

    exercícios complementares

---
+++ Exercícios Matriz/Exercícios Complementares/exer01.py
@@ -0,0 +1,6 @@
+"""
+1. Preencha uma matriz 5x5 com números aleatórios (de 1 a 25) de forma que nenhum número se repita na matriz, ou seja 
+caso o número sorteado já esteja contido na matriz, outro número deve ser sorteado 
+
+"""
+
